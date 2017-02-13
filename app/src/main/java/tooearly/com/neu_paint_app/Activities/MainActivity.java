46ff083af9c00dfcf22ecdb3f41f47db9db1066e
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-13 07:38:29 -0700

    feat(paint-view): add custom PaintView and implement simple gestures

--- app/src/main/java/tooearly/com/neu_paint_app/Activities/MainActivity.java
+++ app/src/main/java/tooearly/com/neu_paint_app/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package tooearly.com.neu_paint_app;
+package tooearly.com.neu_paint_app.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import tooearly.com.neu_paint_app.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
