49c55a6b13889bc370e3769180ea0848abfc1048
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-15 09:06:59 -0700

    feat(clear-paint-command): allow users to clear the screen with any brush

--- app/src/main/java/tooearly/com/neu_paint_app/Util/ClearPaintCommand.java
+++ app/src/main/java/tooearly/com/neu_paint_app/Util/ClearPaintCommand.java
@@ -1,15 +1,21 @@
 package tooearly.com.neu_paint_app.Util;
 
+import android.graphics.Paint;
+
 public class ClearPaintCommand extends PaintCommand {
     public ClearPaintCommand(int color) {
+        this(new Paint());
+        this.brush.setColor(color);
+    }
+    public ClearPaintCommand(Paint brush) {
         super("Clear Canvas");
-        this.color = color;
+        this.brush = brush;
     }
 
-    public final int color;
+    public final Paint brush;
 
     @Override
     public void render(PaintFrame frame) {
-        frame.canvas.drawColor(color);
+        frame.canvas.drawPaint(brush);
     }
 }
